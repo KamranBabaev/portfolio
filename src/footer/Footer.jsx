82e7770a3500a11e4a0e012a835f3899e6893276
Kamran Babaev <babaevkamran2094@gmail.com>
2021-08-03 09:41:26 +0300

    refactor

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,22 +1,29 @@
 import styles from './Footer.module.css'
 import stylesContainer from "../common/styles/Container.module.css";
-import icon from './icons.jpg'
+import inst from './instagram.png'
+import link from './linkedin.png'
+import git from './github.png'
 
 export const Footer = () => {
-    return (
-        <div className={styles.footerBlock}>
-            <div className={`${stylesContainer.container} ${styles.skillsContainer}`}>
-                <div className={styles.footerContainer}>
-                    <h3>Камран Бабаев</h3>
-                    <div className={styles.items}>
-                        <img src={icon} alt=''/>
-                        <img src={icon} alt=''/>
-                        <img src={icon} alt=''/>
-                        <img src={icon} alt=''/>
-                    </div>
-                    <div>@2021 Все права защищены</div>
-                </div>
-            </div>
+  return (
+    <div className={styles.footerBlock}>
+      <div className={`${stylesContainer.container} ${styles.skillsContainer}`}>
+        <div className={styles.footerContainer}>
+          <h3>Камран Бабаев</h3>
+          <div className={styles.items}>
+            <a href='https://www.instagram.com/?hl=ru' target='blank'>
+              <img src={inst} alt=''/>
+            </a>
+            <a href='https://github.com/KamranBabaev' target='blank'>
+              <img src={git} alt=''/>
+            </a>
+            <a href='https://www.linkedin.com/feed/' target='blank'>
+              <img src={link} alt=''/>
+            </a>
+          </div>
+          <div>@2021 Все права защищены</div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
